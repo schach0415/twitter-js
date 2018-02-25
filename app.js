062011e363e5d5a0aae42d9cd31bed9fc268fdc3
Dongwoo Kang <schach0415@yahoo.com>
2018-02-24 20:07:00 -0500

    configuring express

--- app.js
+++ app.js
@@ -2,13 +2,31 @@
 
 const express = require('express');
 const app = express(); // creates an instance of an express application
+const chalk = require('chalk');
 
+
+app.use((req, res, next) => {
+    console.log(chalk.magenta(`${req.method} ${req.url} ${res.statusCode}`))
+    next()
+})
 
 
 app.get('/', (req, res, next) => {
     res.send('hello')
 })
 
+app.use('/special/*', (req, res, next) => {
+    res.send('you reached the special area')
+})
+
+
+app.get('/is-anybody-in-there', (req, res, next) => {
+    res.send('I`m here')
+})
+
+app.post('/modernism', (req, res, next) => {
+    res.send('really')
+})
 app.listen(3000, () => {
     console.log('server listening')
 })
