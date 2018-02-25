ecb8743bb7dd165e4d95641071f009136205bcae
Dongwoo Kang <schach0415@yahoo.com>
2018-02-24 20:52:23 -0500

    non-persistent server-side data storage

---
+++ tweetBank.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const _ = require('lodash');
+
+var data = []
+
+function add(name, content){
+    data.push({ name: name, content: content })
+}
+
+function list(){
+    return _.cloneDeep(data)
+}
+
+function find(properties){
+    return _.cloneDeep(_.filter(data, properties))
+}
+
+module.exports = { add, list, find }
+
+// let's generate some fake tweets for testing and styling. 
+const randArrayEl = function(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+  
+  const getFakeName = function() {
+    const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
+    const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+    return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+  };
+  
+  const getFakeTweet = function() {
+    const awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive'];
+    return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+  };
+  
+  for (let i = 0; i < 10; i++) {
+    module.exports.add( getFakeName(), getFakeTweet() );
+  }
+
+  //testing
+  console.log(data)
